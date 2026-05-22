4eb0662b847f1e7aea947a9ce9c68be5881a848f
Aryan raj <matchdham@gmail.com>
2026-05-22 13:35:39 +0530

    Update server.js

--- match-dham-backend/server.js
+++ match-dham-backend/server.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 app.set('trust proxy', 1);
 
 app.use(cors({
-    origin: ['https://matchdham.netlify.app/', 'http://localhost:3000', 'http://localhost:5000'],
+    origin: ['https:/matchdham.netlify.app/', 'http://localhost:3000', 'http://localhost:5000'],
     credentials: true
 }));
 app.use(helmet({
